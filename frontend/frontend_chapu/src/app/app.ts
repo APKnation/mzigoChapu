0a8e6946824cb2527a6cdb13d066d21f95ea3a11
APKnation <atanasikafuka@gmail.com>
2026-09-02 00:22:07 +0300

    changes made

--- frontend/frontend_chapu/src/app/app.ts
+++ frontend/frontend_chapu/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StickyHeader } from './components/sticky-header/sticky-header';
+import { NavbarComponent } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StickyHeader],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
